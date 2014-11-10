3a6e8732f20e0872bef8e5cecce94bce4070b060
Ignacio Calvo <ignacio.calvo@gmail.com>
2014-11-10 20:31:04 +0100

    First commit

---
+++ Source/Icm.MqLogViewer/LogEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Icm.MqLogViewer
+{
+    internal class LogEntry
+    {
+        public DateTime Date { get; set; }
+        public string Level { get; set; }
+        public string AppName { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
---
+++ Source/Icm.MqLogViewer/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Icm.MqLogViewer
+{
+    internal class Program
+    {
+        private static ConsoleColor _defaultBackgroundColor;
+        private static ConsoleColor _defaultForegroundColor;
+
+        private static void Main()
+        {
+            Console.WriteLine("LOGVIEWER 1.0");
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = ConfigurationManager.AppSettings["RabbitMqHost"],
+                UserName = ConfigurationManager.AppSettings["RabbitMqUser"],
+                Password = ConfigurationManager.AppSettings["RabbitMqPassword"],
+                Protocol = Protocols.DefaultProtocol
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel model = connection.CreateModel())
+            {
+                QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+                QueueDeclareOk queue = model.QueueDeclare(
+                    ConfigurationManager.AppSettings["RabbitMqQueue"], 
+                    durable: true, 
+                    exclusive: true, 
+                    autoDelete: true,
+                    arguments: null);
+                model.QueueBind(
+                    queue: queue,
+                    exchange: ConfigurationManager.AppSettings["RabbitMqExchange"],
+                    routingKey: ConfigurationManager.AppSettings["RabbitMqRoutingKey"]);
+                model.BasicConsume(
+                    queue: queue, 
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Started listening");
+
+                _defaultForegroundColor = Console.ForegroundColor;
+                _defaultBackgroundColor = Console.BackgroundColor;
+                while (true)
+                {
+                    BasicDeliverEventArgs message = consumer.Queue.Dequeue();
+                    try
+                    {
+                        string messageBody = message.Body.AsUtf8String();
+                        LogEntry logEntry = JsonConvert.DeserializeObject<LogEntry>(messageBody);
+                        Log(logEntry);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void Log(LogEntry logEntry)
+        {
+            Console.BackgroundColor = _defaultBackgroundColor;
+            switch (logEntry.Level)
+            {
+                case "TRACE":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "DEBUG":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "INFO":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "WARN":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "ERROR":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "FATAL":
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    break;
+            }
+            Console.WriteLine("{0} {1} {2} {3}", logEntry.Date, logEntry.Level, logEntry.Message, logEntry.Exception);
+            Console.ForegroundColor = _defaultForegroundColor;
+            Console.BackgroundColor = _defaultBackgroundColor;
+        }
+    }
+
+    internal static class Extensions
+    {
+        public static string AsUtf8String(this byte[] args)
+        {
+            return Encoding.UTF8.GetString(args);
+        }
+    }
+}
